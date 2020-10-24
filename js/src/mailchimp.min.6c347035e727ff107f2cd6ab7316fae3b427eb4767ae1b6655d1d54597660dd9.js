5b5699033daf2c3edc1d029d0a6ef62a27034e8e
antoninodimaggio <antoninojdimaggio@gmail.com>
2020-10-24 00:23:13 -0400

    Fix clearing of form

---
+++ js/src/mailchimp.min.6c347035e727ff107f2cd6ab7316fae3b427eb4767ae1b6655d1d54597660dd9.js
@@ -0,0 +1,3 @@
+$(function(){$('#subscribe-btn').on('click',(event)=>{event.preventDefault();let $form=$('#mailchimp-form');$.ajax({type:'GET',url:$form.attr('action'),data:$form.serialize(),crossDomain:true,dataType:'jsonp',success:function(result){dealtWithResult(result,$form);}});});});const dealtWithResult=(result,form)=>{let $mcSpan=$('#mc-msg');if(result.result=='success'){$mcSpan.text('Thanks means a lot.')
+form.trigger('reset');}else{let msg=result.msg.split('-')[1]
+$mcSpan.text(msg)}}
