51d2944ef17c14494f5696ad37d28102bb943e57
antoninodimaggio <antoninojdimaggio@gmail.com>
2020-10-24 14:01:50 -0400

    Fix bugs make things cleaner

--- js/src/mailchimp.min.2a17f4fc8cd3fd8ed86fbfbe9e41066f5095fbf3f4481f2ba7421c3709d35730.js
+++
@@ -1,2 +0,0 @@
-$(function(){$('#subscribe-btn').on('click',(event)=>{event.preventDefault();let $form=$('#mailchimp-form');$.ajax({type:'GET',url:$form.attr('action'),data:$form.serialize(),crossDomain:true,dataType:'jsonp',success:function(result){dealtWithResult(result);}});});});const dealtWithResult=(result)=>{let $mcSpan=$('#mc-msg');if(result.result=='success'){$mcSpan.text('Thanks means a lot.')}else{let msg=result.msg.split('-')[1]
-$mcSpan.text(msg)}}
